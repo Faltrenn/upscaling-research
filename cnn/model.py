e8830dbeefb98f577a15826ace5340762a83ae8a
Emanuel <manelzaum@icloud.com>
2025-07-22 20:09:20 -0300

    organize project and change requirements.

--- cnn/model.py
+++ cnn/model.py
@@ -1,6 +1,13 @@
 from keras import models, layers
 from PIL import Image
 import numpy as np
+import tensorflow as tf
+from keras.callbacks import EarlyStopping
+import os
+from datetime import datetime
+
+
+IMAGES_PATH = "../images/"
 
 
 def create_model(input_shape, scale) -> models.Sequential:
@@ -8,11 +15,9 @@ def create_model(input_shape, scale) -> models.Sequential:
     model.add(layers.Input(shape=input_shape))
     model.add(layers.Conv2D(64, (9, 9), activation="relu", padding="same"))
     model.add(layers.Conv2D(32, (1, 1), activation="relu", padding="same"))
-    model.add(layers.Conv2D(3, (5, 5), activation="linear", padding="same"))
 
-    model.add(layers.Flatten())
-    length = input_shape[0] * input_shape[1] * scale * scale * 3
-    model.add(layers.Dense(length))
+    model.add(layers.Conv2D(3 * scale * scale, (5, 5), activation="linear", padding="same"))
+    model.add(layers.Lambda(lambda x: tf.nn.depth_to_space(x, scale)))
 
     return model
 
@@ -33,3 +38,91 @@ def preprocess_image(file_path: str, scale: int) -> np.ndarray:
     image = image.resize((w * scale, h * scale), Image.Resampling.BICUBIC)
 
     return normalize_image(image)
+
+
+def create_training_data(dataset: dict[str, list[str]], scale: int):
+    X_train = []
+    Y_train = []
+
+    for input_image_name, output_images_names in dataset.items():
+        input_image_path = IMAGES_PATH + input_image_name
+
+        output_image_name = None
+        if scale == 2:
+            output_image_name = output_images_names[1]  # 640x480
+        elif scale == 3:
+            output_image_name = output_images_names[2]  # 960x720
+        elif scale == 4:
+            output_image_name = output_images_names[0]  # 1280x960
+
+        if not output_image_name:
+            continue
+
+        output_image_path = IMAGES_PATH + output_image_name
+
+        x = open_normalized_image(input_image_path)
+        y = open_normalized_image(output_image_path)
+
+        X_train.append(x)
+        Y_train.append(y)
+
+    X_train = np.array(X_train)
+    Y_train = np.array(Y_train)
+
+    return X_train, Y_train
+
+
+def predict(model_path:str, scale: int, input_path: str, output_filename: str):
+    model_filename = os.path.basename(model_path)
+
+    PREDICTS_MODEL_PATH = f"predicts/{model_filename[:-6]}/"
+    os.makedirs(PREDICTS_MODEL_PATH, exist_ok=True)
+
+    model = models.load_model(model_path)
+
+    input_image = open_normalized_image(input_path)
+    input_image = np.expand_dims(input_image, axis=0)
+
+    print(f"Upscaling image {input_path} in {scale}x")
+
+    predicted_image = np.squeeze(np.clip(model.predict(input_image) * 255, 0, 255))  # type: ignore
+    predicted_image = predicted_image.astype(np.uint8)
+
+    output_image = Image.fromarray(predicted_image).convert("RGB")
+    output_image.save(PREDICTS_MODEL_PATH + output_filename)
+
+
+def get_formatted_time() -> str:
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+def train(model_name:str, epochs: int, scale: int, patience: int = 50):
+    IMAGES_IN_PATH = sorted(
+        [file for file in os.listdir(IMAGES_PATH) if file.endswith(".png")]
+    )
+
+    images_names = list(set(["_".join(image.split("_")[:-1]) for image in IMAGES_IN_PATH]))
+
+    dataset: dict[str, list[str]] = {}
+
+    for image_name in images_names:
+        input = f"{image_name}_320x240.png"
+        dataset[input] = [
+            image
+            for image in IMAGES_IN_PATH
+            if "_".join(image.split("_")[:-1]) == image_name and image != input
+        ]
+
+    early_stop = EarlyStopping(
+        monitor="loss", patience=patience, restore_best_weights=True
+    )
+
+    X_train, Y_train = create_training_data(dataset, scale)
+    srcnn_model = create_model(X_train.shape[1:], scale)
+    srcnn_model.compile(optimizer="adam", loss="mean_squared_error")
+    srcnn_model.fit(
+        X_train, Y_train, epochs=epochs, batch_size=16, callbacks=[early_stop]
+    )
+    srcnn_model.save(f"models/{scale}x_{epochs}_{model_name}_{get_formatted_time()}.keras")
+
+    quit(0)
--- cnn/upscaling_ia.py
+++ cnn/upscaling_ia.py
@@ -4,15 +4,26 @@ import numpy as np
 from PIL import Image
 from sys import argv
 
+from model import (
+    create_model,
+    open_normalized_image,
+    preprocess_image,
+    IMAGES_PATH,
+    train,
+    predict,
+)
+
 
 def wrong_usage():
     print("WRONG USAGE!")
-    print(f"{argv[0]} mode scale model_path/epochs input_path outputh_path")
+    print(f"{argv[0]} mode scale model_path/epochs input_path/model_name output_filename")
     print("scale can be 2, 3 or 4.")
-    print("mode can be predict or train.")
-    print("\tif train, epochs needs to be passed instead model_path.")
+    print("mode can be predict, predict_all and train.")
+    print("\tif train, epochs and model_name needs to be passed instead model_path and input_path respectively.")
     print("\tinput_path are ignored.")
-    print("\tif predict, model_path, input_path and output_path are needed.")
+    print("\tif predict, model_path, input_path and output_filename are needed.")
+    print("\tif predict_all, just mode and scale are needed.")
+    print("\tpredict all predicts all low images in all trained images.")
     quit(1)
 
 
@@ -26,111 +37,37 @@ scale = int(argv[2])
 
 if (
     scale not in (2, 3, 4)
-    or mode not in ("predict", "train")
+    or mode not in ("predict", "predict_all", "train")
     or (mode == "predict" and argc < 6)
 ):
     wrong_usage()
 
-# Import keras takes too long time, so, importing after check args.
-from model import (
-    create_model,
-    open_normalized_image,
-    preprocess_image,
-)
-
-
-IMAGES_PATH = "../images/"
-IMAGES_IN_PATH = sorted(
-    [file for file in os.listdir(IMAGES_PATH) if file.endswith(".png")]
-)
-
-images_names = list(set(["_".join(image.split("_")[:-1]) for image in IMAGES_IN_PATH]))
-
-dataset: dict[str, list[str]] = {}
-
-for image_name in images_names:
-    input = f"{image_name}_320x240.png"
-    dataset[input] = [
-        image
-        for image in IMAGES_IN_PATH
-        if "_".join(image.split("_")[:-1]) == image_name and image != input
-    ]
-
-
-def create_training_data(dataset: dict[str, list[str]], scale: int):
-    X_train = []
-    Y_train = []
-
-    for input_image_name, output_images_names in dataset.items():
-        input_image_path = IMAGES_PATH + input_image_name
-
-        output_image_name = None
-        if scale == 2:
-            output_image_name = output_images_names[1]  # 640x480
-        elif scale == 3:
-            output_image_name = output_images_names[2]  # 960x720
-        elif scale == 4:
-            output_image_name = output_images_names[0]  # 1280x960
-
-        if not output_image_name:
-            continue
-
-        output_image_path = IMAGES_PATH + output_image_name
-
-        x = open_normalized_image(input_image_path)
-        y = open_normalized_image(output_image_path)
-
-        X_train.append(x)
-        Y_train.append(y)
-
-    X_train = np.array(X_train)
-    Y_train = np.array(Y_train)
-
-    return X_train, Y_train
-
-
 if mode == "train":
-    from datetime import datetime
-    from keras.callbacks import EarlyStopping
-
-    def get_formatted_time() -> str:
-        return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    if argc < 5:
+        wrong_usage()
 
     epochs = int(argv[3])
-
-    early_stop = EarlyStopping(
-        monitor="val_loss", patience=15, restore_best_weights=True
-    )
-
-    X_train, Y_train = create_training_data(dataset, scale)
-    srcnn_model = create_model(X_train.shape[1:], scale)
-    srcnn_model.compile(optimizer="adam", loss="mean_squared_error")
-    srcnn_model.fit(
-        X_train, Y_train, epochs=epochs, batch_size=16, callbacks=[early_stop]
-    )
-    srcnn_model.save(f"models/{scale}x_{epochs}_{get_formatted_time()}.keras")
-
+    model_name = argv[4]
+    train(model_name, epochs, scale)
     quit(0)
 
-model_path = argv[3]
-input_path = argv[4]
-output_path = argv[5]
+if mode == "predict":
+    model_path = argv[3]
+    input_path = argv[4]
+    output_filename = argv[5]
 
-model = models.load_model(model_path)
+    predict(model_path, scale, input_path,  output_filename)
+    quit(0)
 
-input_image = preprocess_image(input_path, scale)
-input_image = np.expand_dims(input_image, axis=0)
+if mode == "predict_all":
+    MODELS_PATH = "models/"
+    models = sorted([MODELS_PATH + file for file in os.listdir(MODELS_PATH) if file.endswith(".keras")])
 
-print(f"Upscaling image {input_path} in {scale}x")
+    LOW_IMAGES = sorted(
+        [IMAGES_PATH + file for file in os.listdir(IMAGES_PATH) if "320x240" in file]
+    )
 
-predicted_image = np.squeeze(np.clip(model.predict(input_image) * 255, 0, 255))  # type: ignore
-predicted_image = predicted_image.astype(np.uint8)
-
-output_image = Image.fromarray(predicted_image).convert("RGB")
-output_image.show()
-output_image.save(output_path)
-
-print(f"Input shape: {input_image.shape}")
-print(f"Predicted shape: {predicted_image.shape}")
-
-Image.open(input_path).convert("RGB").show()
+    for model_path in models:
+        print(f"USING MODEL {model_path}")
+        for i, low_image in enumerate(LOW_IMAGES):
+            predict(model_path, scale, low_image, f"{i}.png")
